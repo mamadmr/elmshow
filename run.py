ca89b11e0881fcf65ea680ad6eaf46bd53038aea
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-10 15:26:13 +0330

    add money request for park

--- run.py
+++ run.py
@@ -18,6 +18,7 @@ from src.blueprints.return_question import return_question_blue
 from src.blueprints.return_question_with_answer import return_question_with_answer_blue
 from src.blueprints.enter_score import enter_score_blue
 from src.blueprints.edit_score import edit_score_blue
+from src.blueprints.add_money import add_money_blue 
 
 # register blueprints
 app.register_blueprint(index_blue)
@@ -26,7 +27,7 @@ app.register_blueprint(return_question_blue)
 app.register_blueprint(return_question_with_answer_blue)
 app.register_blueprint(enter_score_blue)
 app.register_blueprint(edit_score_blue)
-
+app.register_blueprint(add_money_blue)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/blueprints/add_money.py
@@ -0,0 +1,42 @@
+from flask import Blueprint
+from src.tools.check_login import check_login
+from src.tools.check_team import check_team, get_money, get_name
+from src.tools.team_requests import get_all_questions
+from src.tools.check_storage import get_storage_question
+from src.database.connector import run_sql
+from flask import current_app
+import random
+import datetime
+
+from flask import request
+
+add_money_blue = Blueprint('add_money', __name__)
+
+@add_money_blue.route('/add_money', methods=['POST'])
+@check_login
+def add_money_func(user_id):
+    now = datetime.datetime.now()
+    current_time = f'{now.hour}:{now.minute}'
+    input_data = request.form.to_dict()
+    output_dict = {"message": "", "team_name": "", "money": 0}
+
+    if "team_id" not in input_data:
+        output_dict["message"] = "team_id is missing"
+        return output_dict, 400
+    
+    team_id = input_data["team_id"]
+    if not check_team(team_id):
+        output_dict["message"] = "this team does not exist"
+        return output_dict, 400
+    
+    amount = 0
+    if 'amount' in input_data:
+        amount = int(input_data['amount']) 
+
+    # update database
+    run_sql(f""" UPDATE Teams SET money = money + {amount} WHERE id = {team_id}""")
+
+    output_dict['message'] = "success"
+    output_dict['team_name'] = get_name(team_id)
+    output_dict['money'] = run_sql(f""" SELECT money FROM Teams WHERE id = {team_id}""")[0][0]
+    return output_dict, 200
