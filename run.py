62e783eda07e46a542197a5b4943390f73514a5f
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-10 08:32:17 +0330

    add config file as a global variable for blueprint usage

--- run.py
+++ run.py
@@ -1,14 +1,21 @@
 from flask import Flask
+import json
+
+# read config file 
+with open('config.json', 'r') as f:
+    config = json.load(f)
 
 app = Flask(__name__)
 app.app_context().push()
+
+# set config
+app.config['config'] = config
 
 # import blueprints
 from src.blueprints.index import index_blue
 
 # register blueprints
 app.register_blueprint(index_blue)
-
 
 
 if __name__ == '__main__':
