be7ebacc98febc53b54bc00720b336cfbcd93bc4
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-10 14:25:15 +0330

    write edit socre api

--- run.py
+++ run.py
@@ -17,6 +17,7 @@ from src.blueprints.get_question_from_storage import get_question_blue
 from src.blueprints.return_question import return_question_blue
 from src.blueprints.return_question_with_answer import return_question_with_answer_blue
 from src.blueprints.enter_score import enter_score_blue
+from src.blueprints.edit_score import edit_score_blue
 
 # register blueprints
 app.register_blueprint(index_blue)
@@ -24,6 +25,7 @@ app.register_blueprint(get_question_blue)
 app.register_blueprint(return_question_blue)
 app.register_blueprint(return_question_with_answer_blue)
 app.register_blueprint(enter_score_blue)
+app.register_blueprint(edit_score_blue)
 
 
 if __name__ == '__main__':
