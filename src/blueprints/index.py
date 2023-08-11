3870ba7e6375d8d0239f02a13d96e50d0806275f
Mohamad Mahdi Reisi <m.reisi@ec.iut.ac.ir>
2023-08-11 11:44:51 +0330

    add some comments and clean index blueprint

--- src/blueprints/index.py
+++ src/blueprints/index.py
@@ -1,10 +1,16 @@
+"""
+this blueprint is aim for test the connection between frontend and backend
+and check password and username
+"""
+
 from flask import Blueprint
 from src.tools.check_login import check_login
 
 
 index_blue = Blueprint('index', __name__)
 
+# index page 
 @index_blue.route('/', methods=['GET'])
 @check_login
 def index_func(user_id):
-    return "Welcome to ElmShow user " + str(user_id)
+    return "Welcome to ElmShow you are login with user" + str(user_id)
